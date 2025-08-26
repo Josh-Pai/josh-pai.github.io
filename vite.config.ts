26d83a81ffbfcf237317ff4ad3416fad551fb6f6
Josh Pai <mocha1122@gmail.com>
2025-08-26 15:32:43 -0400

    Updated configuration

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/' // Since this will be the base directory
+  base: '/josh-pai.github.io/' // Since this will be the base directory
 }));
